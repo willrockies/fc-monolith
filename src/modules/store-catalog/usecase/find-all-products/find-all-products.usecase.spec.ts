163b81d71546cc0334a4936f7ba9ec8d4c020820
willrockies <wlsn.ads@gmail.com>
2024-02-24 20:36:49 -0300

    implementando teste do findallproducts usecase

---
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.spec.ts
@@ -0,0 +1,46 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Product from "../../domain/product.entity"
+
+const product = new Product({
+    id: new Id("1"),
+    name: "Product 1",
+    description: "Product 1 description",
+    salesPrice: 100
+});
+
+
+const product2 = new Product({
+    id: new Id(""),
+    name: "Product 2",
+    description: "Product 2 description",
+    salesPrice: 200
+});
+
+const MockRepository = () => {
+    return {
+        findAll: jest.fn().mockReturnValue(Promise.resolve([product, product2])),
+        find: jest.fn(),
+    }
+}
+
+
+describe("find all products usecase unit test", () => {
+    it("should find all products", async () => {
+        const productRepository = MockRepository();
+        const usecase = new FindAllProductsUseCase(productRepository);
+
+        const result = await usecase.execute();
+
+        expect(productRepository.findAll).toHaveBeenCalled();
+        expect(result.products.length).toBe(2);
+        expect(result.products[0].id).toBe("1");
+        expect(result.products[0].name).toBe("Product 1");
+        expect(result.products[0].description).toBe("Product 1 description");
+        expect(result.products[0].salesPrice).toBe(100);    
+        expect(result.products[1].id).toBe("2");
+        expect(result.products[1].name).toBe("Product 2");
+        expect(result.products[1].description).toBe("Product 2 description");
+        expect(result.products[1].salesPrice).toBe(200);
+    });
+
+})
