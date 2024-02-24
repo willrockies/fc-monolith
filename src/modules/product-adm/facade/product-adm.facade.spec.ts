ba7a9247c5b8b52e9dc567cf93635f25ed6f9313
willrockies <wlsn.ads@gmail.com>
2024-02-24 20:18:22 -0300

    testando facade para verificação de stock

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -44,12 +44,33 @@ describe("ProductAdmFacade test", () => {
         await productFacade.addProduct(input);
 
         const product = await ProductModel.findOne({ where: { id: "1" } });
-        console.log(product.dataValues)
+
         expect(product).toBeDefined();
         expect(product.dataValues.id).toBe(input.id);
         expect(product.dataValues.name).toBe(input.name);
         expect(product.dataValues.description).toBe(input.description);
         expect(product.dataValues.purchasePrice).toBe(input.purchasePrice);
         expect(product.dataValues.stock).toBe(input.stock);
+    });
+
+    it("should check product stock", async () => {
+       
+        const productFacade = ProductAdmFacadeFactory.create();
+       
+        const input = {
+            id: "1",
+            name: "Product 1",
+            description: "Product 1 description",
+            purchasePrice: 10,
+            stock: 10,
+        };
+
+        await productFacade.addProduct(input);
+
+        const result = await productFacade.checkStock({ productId: "1" });
+
+        expect(result.productId).toBe(input.id);
+        expect(result.stock).toBe(input.stock);
+      
     });
 });
