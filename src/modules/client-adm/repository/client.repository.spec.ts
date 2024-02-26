abacfca519b08c5a0a8f1ee645809e1f669185d8
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-02-26 17:24:55 -0300

    Criando ClientModel

---
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize-typescript";
+import { ClientModel } from "./client.model";
+
+describe("ClientRepository test", () => {
+    let sequelize: Sequelize;
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true },
+        });
+
+        await sequelize.addModels([ClientModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+ 
+
+
+});
