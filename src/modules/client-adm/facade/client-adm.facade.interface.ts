4097b534eba6f45805706ba98ca765a1f562e229
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-03-05 17:44:34 -0300

    Definindo interface da facade

---
+++ src/modules/client-adm/facade/client-adm.facade.interface.ts
@@ -0,0 +1,26 @@
+export interface AddClientFacadeInputDto {
+    id?: string
+    name: string;
+    email: string;
+    address: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface FindClientFacadeInputDto {
+    id: string;
+  }
+  
+  export interface FindClientFacadeOutputDto {
+    id: string;
+    name: string;
+    email: string;
+    address: string;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  export default interface ClientAdmFacadeInterface {
+    add(input: AddClientFacadeInputDto): Promise<void>;
+    find(input: FindClientFacadeInputDto): Promise<FindClientFacadeOutputDto>;
+  }
