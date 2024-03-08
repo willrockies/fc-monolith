e382574e98cac1a1037d24bc538a2539d79de79e
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-03-08 17:07:34 -0300

    iniciando com payment

---
+++ src/modules/payment/domain/transactions.ts
@@ -0,0 +1,31 @@
+import AggregateRoot from "../../@shared/domain/entity/aggregate-root.interface";
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+import Id from "../../@shared/domain/value-object/id.value-object"
+
+type TransactionProps = {
+    id?: Id;
+    amount: number;
+    orderId: string;
+    status?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+
+export default class Transaction extends BaseEntity implements AggregateRoot {
+    private _amount: number;
+    private _orderId: string;
+    private _status: string;
+    private _createdAt: Date;
+    private _updatedAt: Date;
+
+    constructor(props: TransactionProps) {
+        super(props.id);
+        this._amount = props.amount;
+        this._orderId = props.orderId;
+        this._status = props.status || "pending";
+
+    }
+
+   
+}
