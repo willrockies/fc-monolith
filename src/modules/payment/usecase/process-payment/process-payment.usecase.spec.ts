7a8df63cc371968f2918b02a941211ba4d5ca555
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-03-11 11:56:15 -0300

    finalizando teste do caso de uso

--- src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
+++ src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
@@ -7,6 +7,7 @@ const transaction = new Transaction({
     id: new Id("1"),
     amount: 100,
     orderId: "1",
+    status: "approved"
 });
 
 const MockRepository = () => {
@@ -14,7 +15,18 @@ const MockRepository = () => {
         save: jest.fn().mockReturnValue(Promise.resolve(transaction)),
     }
 }
+const transaction2 = new Transaction({
+    id: new Id("1"),
+    amount: 50,
+    orderId: "1",
+    status: "declined",
+});
 
+const MockRepositoryDeclined = () => {
+    return {
+        save: jest.fn().mockReturnValue(Promise.resolve(transaction2)),
+    };
+};
 describe("Process payment usecase unit test", () => {
     it("should approve a transaction", async () => {
         const paymentRepository = MockRepository();
@@ -36,6 +48,22 @@ describe("Process payment usecase unit test", () => {
     });
 
     it("should decline a transaction", async () => {
+        const paymentRepository = MockRepositoryDeclined();
+        const usecase = new ProcessPaymentUseCase(paymentRepository);
+        const input = {
+            orderId: "1",
+            amount: 50,
+        };
+
+        const result = await usecase.execute(input);
+
+        expect(result.transactionId).toBe(transaction2.id.id);
+        expect(paymentRepository.save).toHaveBeenCalled();
+        expect(result.status).toBe("declined");
+        expect(result.amount).toBe(50);
+        expect(result.orderId).toBe("1");
+        expect(result.createdAt).toBe(transaction2.createdAt);
+        expect(result.updatedAt).toBe(transaction2.updatedAt);
     });
 
 
--- src/modules/payment/usecase/process-payment/process-payment.usecase.ts
+++ src/modules/payment/usecase/process-payment/process-payment.usecase.ts
@@ -20,7 +20,7 @@ export default class ProcessPaymentUseCase implements UseCaseInterface {
             transactionId: persistTransaction.id.id,
             orderId: persistTransaction.orderId,
             amount: persistTransaction.amount,
-            status: transaction.status,
+            status: persistTransaction.status,
             createdAt: persistTransaction.createdAt,
             updatedAt: persistTransaction.updatedAt,
         };
