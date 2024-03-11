6886e324104b917f69cdf5247776c9ab2ea84422
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-03-11 10:37:20 -0300

    finalizando criação da entidade

--- src/modules/payment/domain/transactions.ts
+++ src/modules/payment/domain/transactions.ts
@@ -16,16 +16,50 @@ export default class Transaction extends BaseEntity implements AggregateRoot {
     private _amount: number;
     private _orderId: string;
     private _status: string;
-    private _createdAt: Date;
-    private _updatedAt: Date;
+
 
     constructor(props: TransactionProps) {
         super(props.id);
         this._amount = props.amount;
         this._orderId = props.orderId;
         this._status = props.status || "pending";
+        this.validate();
 
     }
 
-   
+    validate(): void {
+        if (this._amount <= 0) {
+            throw new Error("Amount must be greater than 0");
+        }
+    }
+
+    approve(): void {
+        this._status = "approved";
+
+    }
+
+    decline(): void {
+        this._status = "declined";
+    }
+
+    process(): void {
+        if (this._amount >= 100) {
+            this.approve();
+        } else {
+            this.decline();
+        }
+    }
+
+    get amount(): number {
+        return this._amount;
+    }
+
+    get orderId(): string {
+        return this._orderId;
+    }
+
+    get status(): string {
+        return this._status;
+    }
+
 }
